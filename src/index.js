e46944a62fb85b586a0bae3a2af20944bce70c59
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-12 22:04:22 +0500

    Added a heart icon to the movie

--- src/index.js
+++ src/index.js
@@ -8,3 +8,36 @@ const fetchData = async () => {
   console.log(result);
   return result;
 };
+
+const homepage = document.querySelector("#homepage");
+
+const renderMovies = async () => {
+  homepage.innerHTML = "";
+
+  try {
+    const movies = await fetchData();
+
+    movies.forEach((movie) => {
+      homepage.innerHTML += `
+      <div class="movie">
+      <img src=${movie.image.medium} class="movie-image" alt="Movie Poster">
+      <div class="movie-info">
+        <h4 class="movie-name">${movie.name}</h4>
+        <div class="movie-likes">
+        <i class="fa fa-heart-o" aria-hidden="true"></i>
+        <p id="${movie.id}" >Likes</p>
+        </div>
+        
+    </div>
+    <div class="button-container">
+        <a id="${movie.id}" href=${movie.url} class="watch-button">Watch</a>
+        <button id="${movie.id}" class="comment-button">Comment</button>
+    </div>
+      </div>`;
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+renderMovies();
