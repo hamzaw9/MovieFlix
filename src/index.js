a26b4c8d83ab440b9c00e55ef4c46534d41e07a9
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-13 22:15:12 +0500

    Style navbar and main section for improved responsiveness and visual appeal

--- src/index.js
+++ src/index.js
@@ -1,90 +1,6 @@
-/* import './style.css';
+import "./style.css";
 
-const moviesURL = 'https://api.tvmaze.com/shows';
-
-const fetchData = async () => {
-  const response = await fetch(moviesURL);
-  const result = await response.json();
-  console.log(result);
-  return result;
-};
-
-const homepage = document.querySelector('#homepage');
-
-const renderMovies = async () => {
-  homepage.innerHTML = '';
-
-  try {
-    const movies = await fetchData();
-
-    movies.forEach((movie) => {
-      homepage.innerHTML += `
-      <div class="movie">
-      <img src=${movie.image.medium} class="movie-image" alt="Movie Poster">
-      <div class="movie-info">
-        <h4 class="movie-name">${movie.name}</h4>
-        <div class="movie-likes">
-        <i class="fa fa-heart-o" aria-hidden="true"></i>
-        <p id="${movie.id}" >Likes</p>
-        </div>
-
-    </div>
-    <div class="button-container">
-        <a id="${movie.id}" href=${movie.url} class="watch-button">Watch</a>
-        <button id="${movie.id}" class="comment-button">Comment</button>
-    </div>
-      </div>`;
-    });
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
-
-renderMovies();
-
-function moviePopup () {
-  const div = document.getElementById('homepage');
-
-  div.forEach(movie => {
-  `<section class="popup_container">
-      <div class="popup_img ">
-      <img src="${movie.image.medium}" alt="movieImg">
-      </div>
-      <h2 class="movieTitle">${movie.name}</h2>
-      <ul>
-        <li>
-          <p class="movie_actor">Name: <span id="movie_actor">the rock</span> </p>
-        </li>
-        <li>
-          <p class="movie_actor">Name: <span id="movie_actor">the rock</span> </p>
-        </li>
-        <li>
-          <p class="movie_actor">Name: <span id="movie_actor">the rock</span> </p>
-        </li>
-      </ul>
-      <h4>Comments: <span id="commentsCount"></span></h4>
-     <article id="comments">
-
-     </article>
-
-     <h3>Add comments</h3>
-     <form action="" id="form">
-       <input id="name" type="text" name="name" placeholder="Your name"> <br>
-       <textarea name="comments" id=
-       "movie_comments" cols="30" rows=
-       "10" placeholder="Your insights"></textarea><br>
-       <button type="submit">Comment</button>
-     </form>
-
-    </section>`
-
-  })
-
-}
-*/
-import './style.css';
-
-const moviesURL = 'https://api.tvmaze.com/shows';
+const moviesURL = "https://api.tvmaze.com/shows";
 
 const fetchData = async () => {
   const response = await fetch(moviesURL);
@@ -92,12 +8,12 @@ const fetchData = async () => {
   return result;
 };
 
-const homepage = document.querySelector('#homepage');
-const popupContainer = document.querySelector('#popup-container');
-const overlay = document.getElementById('overlay');
+const homepage = document.querySelector("#homepage");
+const popupContainer = document.querySelector("#popup-container");
+const overlay = document.getElementById("overlay");
 
 function closebuttonHandler() {
-  popupContainer.style.display = 'none';
+  popupContainer.style.display = "none";
 }
 
 function showPopup(movie) {
@@ -112,11 +28,7 @@ function showPopup(movie) {
       <h4>Comments: <span id="commentsCount"></span></h4>
      <article id="comments" class="comments_container">
 
-
-     
-
      </article>
-
      <h3>Add comments</h3>
      <form action="" id="form">
        <input id="name" type="text" name="name" placeholder="Your name"> <br>
@@ -125,56 +37,55 @@ function showPopup(movie) {
      </form>
     </section>`;
   // Display the popup
-  popupContainer.style.display = 'block';
-  overlay.style.display = 'block';
+  popupContainer.style.display = "block";
+  overlay.style.display = "block";
 
-  const closeBtn = document.getElementById('close_popup');
-  closeBtn.addEventListener('click', closebuttonHandler);
+  const closeBtn = document.getElementById("close_popup");
+  closeBtn.addEventListener("click", closebuttonHandler);
 }
 
-// Add event listener to close the popup when clicking outside of it
-document.addEventListener('click', (event) => {
+// close popup
+document.addEventListener("click", (event) => {
   if (event.target === popupContainer) {
-    popupContainer.style.display = 'none';
+    popupContainer.style.display = "none";
   }
 });
 
 const renderMovies = async () => {
-  homepage.innerHTML = '';
+  homepage.innerHTML = "";
 
   try {
     const movies = await fetchData();
 
     movies.forEach((movie) => {
       homepage.innerHTML += `
-      <p id ="error_message"></p>
       <div class="movie">
         <img src=${movie.image.medium} class="movie-image" alt="Movie Poster">
         <div class="movie-info">
           <h4 class="movie-name">${movie.name}</h4>
           <div class="movie-likes">
-            <i class="fa fa-heart-o" aria-hidden="true"></i>
-            <p id="${movie.id}" >Likes</p>
+            <i class="fa fa-heart-o like-icon" aria-hidden="true"></i>
+            <p id="${movie.id}"><span class="display-likes"></span> Likes</p>
           </div>
         </div>
         <div class="button-container">
-          <a id="${movie.id}" href=${movie.url} class="watch-button">Watch</a>
+          <a id="${movie.id}" target="_blank" href=${movie.url} class="watch-button">Watch</a>
           <button id="${movie.id}" class="comment-button">Comment</button>
         </div>
       </div>`;
     });
 
     // Add event listeners to comment buttons
-    const commentButtons = document.querySelectorAll('.comment-button');
+    const commentButtons = document.querySelectorAll(".comment-button");
     commentButtons.forEach((button) => {
-      button.addEventListener('click', (event) => {
-        const movieId = event.target.id; // Get the movie ID from the button
-        const movie = movies.find((m) => m.id == movieId); // Find the corresponding movie
-        showPopup(movie); // Call the showPopup function with the movie object
+      button.addEventListener("click", (event) => {
+        const movieId = event.target.id;
+        const movie = movies.find((m) => m.id == movieId);
+        showPopup(movie);
       });
     });
   } catch (error) {
-    const message = document.getElementById('error_message');
+    const message = document.getElementById("error_message");
     message.innerHTML = error;
   }
 };
