6a2df62fd1edeff901e3102eb10fc2084ac16e19
Hamza Baig <mail.hamzabaig@gmail.com>
2023-09-16 01:09:43 +0500

    Fix linter errors

--- src/modules/commentsCount.test.js
+++ src/modules/commentsCount.test.js
@@ -1,13 +1,14 @@
-import commentsCount from "./commentsCount.js";
-import fetchComments from "./fetchComment.js";
-jest.mock("./fetchComment.js", () => ({
+import commentsCount from './commentsCount.js';
+import fetchComments from './fetchComment.js';
+
+jest.mock('./fetchComment.js', () => ({
   __esModule: true,
   default: jest.fn(),
 }));
-test("commentsCount handles case with no comments available", async () => {
-  const commentsCountElement = {textContent: ""};
+test('commentsCount handles case with no comments available', async () => {
+  const commentsCountElement = { textContent: '' };
   document.getElementById = jest.fn(() => commentsCountElement);
   fetchComments.mockResolvedValueOnce(null);
   await commentsCount(456);
-  expect(commentsCountElement.textContent).toBe("No comments available");
+  expect(commentsCountElement.textContent).toBe('No comments available');
 });
