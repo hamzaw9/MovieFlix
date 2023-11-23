6ba0bb64bc215c4fdb230e92750de7b8e5676491
Hamza Baig <mail.hamzabaig@gmail.com>
2023-11-23 17:10:05 +0500

    Limit API response

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -3,7 +3,7 @@ import { moviesURL } from './apiCollection.js';
 const fetchData = async () => {
   const response = await fetch(moviesURL);
   const result = await response.json();
-  return result;
+  return result.slice(0, 40);
 };
 
 export default fetchData;
